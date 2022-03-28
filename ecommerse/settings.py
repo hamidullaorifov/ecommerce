d9bd8bca922929a0be1aa3965fc0ab3f223c1cf9
name <email>
2022-03-28 20:38:51 +0500

    Added STATICFILES_STORAGE attribute to settings

--- ecommerse/settings.py
+++ ecommerse/settings.py
@@ -131,6 +131,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
